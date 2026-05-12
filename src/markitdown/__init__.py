6fbdb73e9a7d6420ae9885a10204760c7c1c09af
Paul Cribbs <paul17crib@users.noreply.github.com>
2026-05-12 11:45:51 +0800

    Add original project link and author attribution to __init__.py

--- src/markitdown/__init__.py
+++ src/markitdown/__init__.py
@@ -1,9 +1,12 @@
 """MarkItDown - Convert various file formats to Markdown.
 
 A fork of microsoft/markitdown with additional features and improvements.
+
+Original project: https://github.com/microsoft/markitdown
 """
 
 from markitdown._markitdown import MarkItDown, DocumentConverter, ConversionResult
 
 __version__ = "0.1.0"
+__author__ = "microsoft/markitdown contributors"
 __all__ = ["MarkItDown", "DocumentConverter", "ConversionResult"]
